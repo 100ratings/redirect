5a7e69bae1a3256abf10f7cc04aaaadab749db93
100ratings <omentalistademian@gmail.com>
2026-04-08 20:58:41 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,17 @@
+const CACHE_NAME = 'pil-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('pil-v2').then((cache) => cache.addAll([
-      'index.html',
-      'manifest.json'
-    ]))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
